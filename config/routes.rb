10ff527dec3751a58d1628829c17e91dba14e4b3
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2018-10-19 22:00:27 +0200

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
       get :validate
     end
   end
+
+  root 'emails#validate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
